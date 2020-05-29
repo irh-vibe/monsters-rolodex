abec4dd7dbefea9ba4d2c9521cdb1f29190098d2
Ian <ian.horton@vibepay.com>
2020-05-29 17:53:48 +0100

    adding files for github pages

---
+++ docs/precache-manifest.05437bbf476b9a3b3d53c4def1665f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf3ba075d05cc4e72666ca088e4352d",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "037800c92821255814ae",
+    "url": "/monstors-rolodex/static/css/main.c7f09e4d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstors-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstors-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037800c92821255814ae",
+    "url": "/monstors-rolodex/static/js/main.9ad757b1.chunk.js"
+  },
+  {
+    "revision": "8cfa0c5cf063f7a2c523",
+    "url": "/monstors-rolodex/static/js/runtime-main.5e963daa.js"
+  }
+]);
